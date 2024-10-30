37224f77fc1d7536b67a7104410e69a8e54b2a9a
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-30 17:23:22 +0200

    Добавьте файлы проекта.

---
+++ Lesson_32_C++/Lesson_32_C++.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace Olhovskyi{
+	class MyClass {
+	public:
+		static void  showInfo() {
+			cout << "Namespace Olhovskyi: MyClass" << endl;
+		}
+	};
+}
+
+namespace Lukashov {
+	class MyClass {
+	public:
+		static void  printInfo() {
+			cout << "Namespace Lukashov: MyClass" << endl;
+		}
+	};
+}
+
+int main()
+{
+	/*MyClass a; - Error*/ 
+	Olhovskyi::MyClass a;
+	a.showInfo();
+	Lukashov::MyClass b;
+	b.printInfo();
+	using namespace Olhovskyi;
+	MyClass c;
+	c.showInfo();
+	using namespace Lukashov;
+	Lukashov::MyClass d;
+}
